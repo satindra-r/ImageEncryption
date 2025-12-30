e063bed26216ccfdddb05d8bdc90c7cd026c5159
Satindra Ramalingam <satindra.r@gmail.com>
2025-12-30 10:59:18 +0530

    fix clamping

--- ImageEncryption.py
+++ ImageEncryption.py
@@ -3,8 +3,6 @@ import scipy.fftpack as fft
 import PIL.Image as Image
 from Crypto.Cipher import AES
 
-fixed_point = 2 ** 15
-
 
 def dct2(a):
 	return fft.dct(fft.dct(a.T, norm='ortho').T, norm='ortho')
@@ -20,60 +18,57 @@ def encrypt(key, img, scale):
 
 	# pad image width to multiple of 4 for AES
 	size = dct_img.shape
-	dct_img = np.pad(dct_img, ((0, 0), (0, (-size[1] % 4))))
+	dct_img = np.pad(dct_img, ((0, 0), (0, (-size[1] % 8))))
 	size = dct_img.shape
 
-	# convert to fixed point for AES encryption
-	dct_img_fixed = np.clip(dct_img * fixed_point, -2 ** 63, 2 ** 63 - 1).astype(np.int64)
-
-	# encrypt only upper 32 bits as lower 32 bits may change due to floating point errors
-	dct_img_fixed_msb = (dct_img_fixed >> 32).astype(np.int32)
-	dct_img_fixed_lsb = dct_img_fixed.astype(np.uint32)
-	dct_img_fixed_encrypted_msb = np.zeros(size).astype(np.int32)
+	dct_img_int_encrypted = np.full(size, 0x3FE << 52 | 0x1 << 34, dtype=np.uint64)
+	cipher = AES.new(key, AES.MODE_ECB)
 	for i in range(size[0]):
-		dct_bytes = dct_img_fixed_msb[i].tobytes()
-		# TODO: implement 2D CBC for more security
-		cipher = AES.new(key, AES.MODE_ECB)
-		cipher_bytes = cipher.encrypt(dct_bytes)
-		dct_img_fixed_encrypted_msb[i] = np.frombuffer(cipher_bytes, dtype=np.int32)
-	dct_img_fixed_encrypted = dct_img_fixed_encrypted_msb.astype(np.int64) << 32 | dct_img_fixed_lsb.astype(np.int64)
+		for j in range(size[1] // 8):
+			data = [np.uint64((dct_img[i][j * 8 + k].view(np.uint64) >> 48) & 0xFFFF) for k in range(8)]
 
-	dct_img_encrypted = dct_img_fixed_encrypted.astype(np.float128) / fixed_point
+			plain_bytes = np.array(data, dtype=np.uint16).tobytes()
+			cipher_bytes = cipher.encrypt(plain_bytes)
 
-	# simulate a scale down, in reality this would be done to the encrypted image
-	dct_img_encrypted = dct_img_encrypted[:size[0] // scale, :]
-	dct_img_encrypted = dct_img_encrypted[:, :size[1] // scale]
+			data_encrypted = np.frombuffer(cipher_bytes, dtype=np.uint16).astype(np.uint64)
+
+			for k in range(8):
+				dct_img_int_encrypted[i][j * 8 + k] |= data_encrypted[k] << 36
+
+	dct_img_encrypted = np.frombuffer(dct_img_int_encrypted.tobytes(), dtype=np.float64).copy().reshape(*size)
 
 	# add bias back to image
 	img_encrypted = idct2(dct_img_encrypted) + 127
-	return img_encrypted
+	return img_encrypted, dct_img_encrypted
 
 
-def decrypt(key, img_encrypted):
+def decrypt(key, img_encrypted, dct):
 	# subtract 127 so values are centered around 0
 	dct_img_encrypted = dct2(img_encrypted - 127)
 
+	b = (dct_img_encrypted - dct)
+	r = (dct_img_encrypted - dct) / dct
 	# remove columns to multiple of 4 for AES as padding will cause changes in last few frequencies while decrypting
 	size = dct_img_encrypted.shape
-	if (size[1] % 4):
-		dct_img_encrypted = dct_img_encrypted[:, :-(size[1] % 4)]
+	if (size[1] % 8):
+		dct_img_encrypted = dct_img_encrypted[:, :-(size[1] % 8)]
 	size = dct_img_encrypted.shape
 
-	# convert to fixed point for AES decryption
-	dct_img_fixed_encrypted = np.clip(dct_img_encrypted * fixed_point, -2 ** 63, 2 ** 63 - 1).astype(np.int64)
+	dct_img_int = np.zeros(size, dtype=np.uint64)
+	cipher = AES.new(key, AES.MODE_ECB)
+	for i in range(size[0]):
+		for j in range(size[1] // 8):
+			data_encrypted = [np.uint64((dct_img_encrypted[i][j * 8 + k].view(np.uint64) >> 36) & 0xFFFF) for k in
+							  range(8)]
 
-	# decrypt only upper 32 bits
-	dct_img_fixed_encrypted_msb = (dct_img_fixed_encrypted >> 32).astype(np.int32)
-	dct_img_fixed_lsb = dct_img_fixed_encrypted.astype(np.uint32)
-	dct_img_fixed_msb = np.zeros(size).astype(np.int64)
-	for i in range(dct_img_fixed_encrypted_msb.shape[0]):
-		cipher_bytes = dct_img_fixed_encrypted_msb[i].tobytes()
-		cipher = AES.new(key, AES.MODE_ECB)
-		dct_bytes = cipher.decrypt(cipher_bytes)
-		dct_img_fixed_msb[i] = np.frombuffer(dct_bytes, dtype=np.int32)
-	dct_img_fixed = dct_img_fixed_msb.astype(np.int64) << 32 | dct_img_fixed_lsb.astype(np.int64)
+			plain_bytes = np.array(data_encrypted, dtype=np.uint16).tobytes()
+			cipher_bytes = cipher.decrypt(plain_bytes)
+			data = np.frombuffer(cipher_bytes, dtype=np.uint16).astype(np.uint64)
 
-	dct_img = dct_img_fixed.astype(np.float128) / fixed_point
+			for k in range(8):
+				dct_img_int[i][j * 8 + k] = data[k] << 48
+	a = dct_img_int.tobytes()
+	dct_img = np.frombuffer(dct_img_int.tobytes(), dtype=np.float64).copy().reshape(*size)
 
 	# add bias back to image
 	img = idct2(dct_img) + 127
@@ -81,23 +76,40 @@ def decrypt(key, img_encrypted):
 
 
 def main():
-	input_path = input("Enter file path:")
-	scale = int(input("Enter scale:"))
-	key = (input("Enter key:") + "\0" * 16)[:16].encode("ascii")
+	input_path = "images/img.png"  # input("Enter file path:")
+	scale = 1  # int(input("Enter scale:"))
+	key = ("1" + "\0" * 16)[:16].encode("ascii")  # (input("Enter key:") + "\0" * 16)[:16].encode("ascii")
 
 	image = Image.open(input_path).convert('L')
-	img = np.array(image).astype(np.float128)
+	img = np.array(image).astype(np.float64)
 
-	img_result = decrypt(key, encrypt(key, img, scale))
+	img_encrypted, a = encrypt(key, img, scale)
 
-	image_result = Image.fromarray(np.clip(img_result, 0, 255).astype(np.uint8))
-	output_path = input_path.replace(".png", "Result.png")
-	image_result.save(output_path)
+	image_encrypted = Image.fromarray(np.round(np.clip(img_encrypted, 0, 255)).astype(np.uint8))
+	output_path = input_path.replace(".png", "Encrypted.png")
+	image_encrypted.save(output_path)
 
-	image_smol = image.resize((image.width // scale, image.height // scale))
+	image_encrypted_scaled = image_encrypted.resize((image_encrypted.width // scale, image_encrypted.height // scale))
+	output_path = input_path.replace(".png", "EncryptedScaled.png")
+	image_encrypted_scaled.save(output_path)
+
+	#img_encrypted_scaled = np.array(image_encrypted_scaled).astype(np.float64)
+	#img_encrypted_scaled = img_encrypted.astype(np.float32).astype(np.float64)
+	img_encrypted_scaled = img_encrypted
+	#print(img_encrypted_scaled[0][0])
+	#img_encrypted_scaled[0][0] = 231.377#np.round(img_encrypted_scaled[0][0])
+	#print(img_encrypted_scaled[0][0])
+
+
+	img_recovered = decrypt(key, img_encrypted_scaled, a)
+
+	image_encrypted = Image.fromarray(np.clip(img_recovered, 0, 255).astype(np.uint8))
+	output_path = input_path.replace(".png", "Recovered.png")
+	image_encrypted.save(output_path)
+
+	'''image_scaled = image.resize((image.width // scale, image.height // scale))
 	output_path = input_path.replace(".png", "Scaled.png")
-	image_smol.save(output_path)
+	image_scaled.save(output_path)'''
 
 
-if (__name__ == "__main__"):
-	main()
+main()
